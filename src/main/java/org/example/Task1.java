5dafcf1c5bf071c3aded36478f810e9a9279c332
KirillZabrodin5 <fea.z@yandex.ru>
2024-10-03 21:35:59 +0500

    fix task1

--- src/main/java/org/example/Task1.java
+++ src/main/java/org/example/Task1.java
@@ -3,14 +3,14 @@ package org.example;
 
 public class Task1 {
     public static void main(String[] args) {
-        int counter = 0;
+        int total = 0;
         int[] a = new int[]{1,2,-3,4,-5,-6,0};
         for(int i = 0; i < a.length; i++){
             if (a[i] > 0) {
-                counter++;
+                total += a[i];
             }
             System.out.print(a[i] + " ");
         }
-        System.out.println("/n" + counter);
+        System.out.println("\n" + total);
     }
 }
